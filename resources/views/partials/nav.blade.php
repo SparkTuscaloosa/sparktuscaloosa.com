7fe8118a4d12b2f6980b990ffc57e88115eb4b74
Wes Eldridge <weseldridge@gmail.com>
2017-07-28 17:32:51 -0500

    updated styling

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,6 +1,14 @@
 <div id="navBar">
-    <a href="{{ route('home') }}">Home</a>
-    <a href="{{ route('events') }}">Upcoming Events</a>
-    {{-- <a href="{{ route('gallery') }}">Gallery</a> --}}
-    <a id="toggleNav" class="icon">&#9776;</a>
+    <div class="nav-wrapper">
+        <div class="brand">
+            <img src="{{ asset('images/small-logo.png') }}" alt="logo" />
+            <h1>Spark Tuscaloosa</h1>
+        </div>
+        <div class="links">
+            <a href="{{ route('home') }}">Home</a>
+            <a href="{{ route('events') }}">Events</a>
+            {{-- <a href="{{ route('gallery') }}">Gallery</a> --}}
+            <a id="toggleNav" class="icon">&#9776;</a>
+        </div>
+    </div>
 </div>
