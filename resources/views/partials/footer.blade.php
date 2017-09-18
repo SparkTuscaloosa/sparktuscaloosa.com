e93fb1e0608bff19674edc25cd787b1254e11465
Ryan Marks <rmarks@gmail.com>
2017-09-18 14:54:56 -0500

    Changed YouTube channel URL to Spark Tuscaloosa.

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -20,7 +20,7 @@
                 <a href="https://twitter.com/sparktuscaloosa" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
                 <a href="https://www.facebook.com/sparktuscaloosa/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
                 <a href="https://www.instagram.com/sparktuscaloosa/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                <a href="https://www.youtube.com/channel/UCo-yOQniEKZ3LDGifnL7cjw" target="_blank"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
+                <a href="https://www.youtube.com/channel/UCo6OzvDm9Ozpl3O-LkMGsaQ" target="_blank"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
                 <a href="https://github.com/SparkTuscaloosa/" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a>
             </div>
         </div>
